bf403467bdef69df127779c0f9e82e853a2b62e1
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-03 22:19:20 +0700

    add multiple error types to error util

---
+++ src/utils/error.ts
@@ -0,0 +1,29 @@
+// src/utils/error.ts
+export class AppError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class BadRequestError extends AppError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string) {
+    super(message, 404);
+  }
+}
+
+export class InternalServerError extends AppError {
+  constructor(message: string = 'Internal server error') {
+    super(message, 500);
+  }
+}
