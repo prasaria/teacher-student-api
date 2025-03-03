8b55e7ef46966a6c7030caec770a81facecc4a88
Prasetya Aria Wibawa <prasetya.ariawibawa@gmail.com>
2025-03-03 22:52:13 +0700

    add extra error type

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -27,3 +27,15 @@ export class InternalServerError extends AppError {
     super(message, 500);
   }
 }
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string = 'Unauthorized') {
+    super(message, 401);
+  }
+}
+
+export class ForbiddenError extends AppError {
+  constructor(message: string = 'Forbidden') {
+    super(message, 403);
+  }
+}
